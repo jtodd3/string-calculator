2071b8dd6d6319639cd37982efd0d335737f0a5b
jtodd3 <jjt796@mail.usask.ca>
2019-10-07 22:12:54 -0600

    add tests to show testing string with newline character(s) in the string

--- src/add.test.js
+++ src/add.test.js
@@ -15,3 +15,11 @@ test('"add" should return a sum given a string of two comma deliminated numbers'
 test('"add" should return a sum given a string of comma deliminated numbers with empty values', () => {
   expect(add('2,,4,6,,,3')).toBe(15);
 });
+
+test('"add" should return a sum given a string of comma deliminated number with new line characters', () => {
+  expect(add('5,3,\n,3,\n,\n,4,\n')).toBe(15);
+});
+
+test('"add" should return a sum given a string of comma deliminated number with new line characters and empty values', () => {
+  expect(add('11,3,,\n,7,\n,3')).toBe(24);
+});
